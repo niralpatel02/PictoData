e9d91c0bc128c13696e94e2a6dcf579372e0cb4a
Niral Patel <niralutm.patel@mail.utoronto.ca>
2020-07-26 19:49:30 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,10 @@
+const express = require('express')
+
+const app = express()
+
+app.use(express.static(__dirname + '/pub'))
+
 const port = process.env.PORT || 5000
 app.listen(port, () => {
-	log(`Listening on port ${port}...`)
+	console.log(`Listening on port ${port}...`)
 })
