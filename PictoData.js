990f8a37e0efc4cfde8f081dfd985a036dbcad1b
Niral Patel <niralutm.patel@mail.utoronto.ca>
2020-07-26 17:07:27 -0400

    Add functional bubble chart

--- PictoData.js
+++ PictoData.js
@@ -11,20 +11,16 @@ Graph.prototype = {
 	    barGraph.style.position = 'relative'
 	    barGraph.style.width = '100%'
         barGraph.style.height = '100%'
-       // barGraph.style.borderLeft = 'solid #000000'
-       //barGraph.style.borderBottom = 'solid #000000'
 
         bars = document.createElement('div')
         bars.style.width = info['width'] + 'px'
         bars.style.height = (info['height']-20) + 'px'
-        //bars.style.marginBottom = '20px'
         bars.style.borderLeft = 'solid #000000'
         bars.style.borderBottom = 'solid #000000'
 
         labels = document.createElement('div')
         labels.style.height = '20px'
         labels.style.width = info['width']
-        //labels.style.paddingTop = '10px'
 
 	    info['Data'].forEach(function(value, index){
             bar = document.createElement('div')
@@ -74,7 +70,58 @@ Graph.prototype = {
 	    barGraph.append(labels)
 		info['Element'].append(barGraph)
 
+        info['type'] = 'bar'
         this.graph = {'object': barGraph, 'info': info}
+	},
+	bubble: function(info){
+
+	    const total = info['Data'].reduce(function(a, b){
+	        return a+b;
+	    })
+
+        const bubbleChart = document.createElement('div')
+        bubbleChart.style.width = '100%'
+        bubbleChart.style.height = '100%'
+
+        info['Data'].forEach(function(value, index){
+            const circle = document.createElement('div')
+            const size = (info['Data'][index]/total)*((info['width'] + info['height'])/2)
+            circle.style.width = size + 'px'
+            circle.style.height = size + 'px'
+            circle.style.borderRadius = '50%'
+            circle.style.backgroundColor = info['colours'][index]
+            circle.style.float = 'left'
+            circle.style.display = 'flex'
+            circle.style.justifyContent = 'center'
+            circle.style.alignItems = 'center'
+            circle.innerHTML = info['Labels'][index]
+
+            hover = document.createElement('span')
+            hover.style.position = 'absolute'
+            hover.style.visibility = 'hidden'
+            hover.style.width = '50px'
+            hover.style.backgroundColor = 'black'
+            hover.style.color = '#fff'
+            hover.style.textAlign = 'center'
+            hover.style.zIndex = "1"
+            hover.innerHTML = info['Data'][index]
+            circle.append(hover)
+
+            circle.onmouseover = function(){
+                this.children[0].style.visibility = 'visible'
+            };
+
+            circle.onmouseout = function() {
+                this.children[0].style.visibility = 'hidden'
+            }
+
+            bubbleChart.append(circle)
+        })
+
+        info['Element'].append(bubbleChart)
+        info['type'] = 'bubble'
+        this.graph = {'object': bubbleChart, 'info': info}
+
 	},
 
 	addData: function(info){
@@ -82,7 +129,12 @@ Graph.prototype = {
 	    this.graph['info']['Labels'] = this.graph['info']['Labels'].concat(info['Labels'])
 	    this.graph['info']['colours'] = this.graph['info']['colours'].concat(info['colours'])
 	    this.graph['info']['Element'].removeChild(this.graph['object'])
-	    this.bar(this.graph['info'])
+	    if (this.graph['info']['type'] === 'bar'){
+	        this.bar(this.graph['info'])
+	    }
+	    else {
+	        this.bubble(this.graph['info'])
+	    }
 	},
 
 	removeData: function(num){
@@ -90,6 +142,11 @@ Graph.prototype = {
 	    this.graph['info']['Labels'] = this.graph['info']['Labels'].filter((element) => this.graph['info']['Labels'].indexOf(element) !== num)
 	    this.graph['info']['colours'] = this.graph['info']['colours'].filter((element) => this.graph['info']['colours'].indexOf(element) !== num)
 	    this.graph['info']['Element'].removeChild(this.graph['object'])
-	    this.bar(this.graph['info'])
+	    if (this.graph['info']['type'] === 'bar'){
+            this.bar(this.graph['info'])
+        }
+        else {
+            this.bubble(this.graph['info'])
+        }
 	}
 }
